4272f364c8843ff4311f2baa1d8ec08ab46e7d2b
Adriano-Porto <adriano.porto.a.j@gmail.com>
2025-03-01 15:45:09 -0300

    refactor: removing unused files

--- save.hs
+++
@@ -1,9 +0,0 @@
--- Salva o jogo num arquivo
-salvaJogo :: Jogo -> IO ()
-salvaJogo jogo = writeFile "meu_ludo.txt" (show jogo)
-
--- Carrega o jogo salvo
-carregaJogo :: IO Jogo
-carregaJogo = do
-  conteudo <- readFile "meu_ludo.txt"
-  return (read conteudo :: Jogo)
