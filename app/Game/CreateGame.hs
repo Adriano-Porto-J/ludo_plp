65490b86f106992423b66ae74fab94135e32dbaa
ArthurVidalRegisGouveia <140714233+ArthurVidalRegisGouveia@users.noreply.github.com>
2025-03-05 22:52:24 -0300

    Update CreateGame.hs

--- app/Game/CreateGame.hs
+++ app/Game/CreateGame.hs
@@ -3,6 +3,7 @@ module Game.CreateGame where
 import Game.Auxiliary
 import GameTypes
 
+-- Cria o estado inicial do jogo com base no número de jogadores e bots
 createGameState :: Int -> Int -> GameState
 createGameState players bots =
   let colors = if players == 2 then [Red, Blue] else if players == 3 then [Red, Yellow, Blue] else [Red, Yellow, Blue, Green]
@@ -11,31 +12,34 @@ createGameState players bots =
       allPieces = concatMap createPieces colors
       specialTiles = createSpecialTiles
    in GameState
-        { players = allPlayers,
-          pieces = allPieces,
-          blockades = [],
-          specialTiles = specialTiles,
-          currentPlayer = Red,
-          diceRolled = -1,
-          processingMove = False,
-          end = False,
-          sixesInRow = 0,
+        { players = allPlayers,       -- Lista de jogadores
+          pieces = allPieces,         -- Lista de peças no tabuleiro
+          blockades = [],             -- Nenhum bloqueio inicialmente
+          specialTiles = specialTiles, -- Casas especiais do tabuleiro
+          currentPlayer = Red,        -- O primeiro jogador sempre será o vermelho
+          diceRolled = -1,            -- O dado ainda não foi rolado
+          processingMove = False,     -- Nenhuma jogada em processamento
+          end = False,                -- O jogo ainda não terminou
+          sixesInRow = 0,             -- Nenhum seis consecutivo ainda
           screenState = MenuInicial,  -- Inicializa o estado da tela como MenuInicial
-          wasLuckyMove = False
+          wasLuckyMove = False        -- Nenhuma jogada de sorte ainda
         }
 
+-- Define quais cores são jogadores e quais são bots
 definePlayers :: [Color] -> Int -> Int -> [(Color, Bool)]
 definePlayers colors numPlayers bots
   | numPlayers + bots > length colors = error "Número de jogadores e bots excede o número de cores disponíveis"
   | otherwise =
       let (playerColors, botColors) = splitAt numPlayers colors
-          playerTuples = map (\color -> (color, False)) playerColors
-          botTuples = map (\color -> (color, True)) botColors
+          playerTuples = map (\color -> (color, False)) playerColors -- Jogadores reais
+          botTuples = map (\color -> (color, True)) botColors -- Bots
        in playerTuples ++ botTuples
 
+-- Cria um jogador com base na cor e se é bot ou não
 createPlayer :: (Color, Bool) -> Player
 createPlayer (color, isB) = Player {playerColor = color, isBot = isB, startingPos = startingPosByColor color}
 
+-- Cria as 4 peças iniciais de um jogador, todas na área inicial
 createPieces :: Color -> [Piece]
 createPieces color =
   [ Piece {pieceId = 1, pieceColor = color, piecePosition = -1, tilesWalked = 0, inStartingArea = True, inFinishArea = False, finished = False},
@@ -44,6 +48,7 @@ createPieces color =
     Piece {pieceId = 4, pieceColor = color, piecePosition = -4, tilesWalked = 0, inStartingArea = True, inFinishArea = False, finished = False}
   ]
 
+-- Define as casas especiais do tabuleiro, como casas seguras, de impulso e de morte
 createSpecialTiles :: [SpecialTile]
 createSpecialTiles =
   [ SpecialTile Safe 0,
