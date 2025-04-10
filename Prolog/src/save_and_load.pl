0483aae064490e98f3bc6f304f220315ba577859
ArthurVidalRegisGouveia <arthur.vidal.regis.gouveia@ccc.ufcg.edu.br>
2025-04-10 00:04:26 -0300

    blockades save fix

--- Prolog/src/save_and_load.pl
+++ Prolog/src/save_and_load.pl
@@ -3,10 +3,6 @@
 :- use_module(library(http/json)).
 :- use_module(library(lists)).
 
-% -----------------------
-% SALVAR O JOGO
-% -----------------------
-
 save_game(FileName, GameState) :-
     game_state_to_serializable(GameState, Serializable),
     setup_call_cleanup(
@@ -16,24 +12,16 @@ save_game(FileName, GameState) :-
     ),
     format('Jogo salvo com sucesso em ~w~n', [FileName]).
 
-% -----------------------
-% CARREGAR O JOGO
-% -----------------------
-
 load_game(FileName, GameState) :-
     setup_call_cleanup(
         open(FileName, read, Stream, [encoding(utf8)]),
         json_read(Stream, Serializable),
         close(Stream)
     ),
-    format('Conteúdo lido do JSON:\n~w\n', [Serializable]),
     serializable_to_game_state(Serializable, GameState),
     format('Jogo carregado com sucesso de ~w~n', [FileName]).
 
-% -----------------------
-% SERIALIZAÇÃO PARA JSON
-% -----------------------
-
+% Converte o estado de jogo em um termo serializável para JSON
 game_state_to_serializable(
     game_state(Players, SpecialTiles, Pieces, Blockades, CurrentPlayer, DiceRolled,
                ProcessingMove, End, SixesInRow, WasLuckyMove, WinnerColor),
@@ -41,7 +29,7 @@ game_state_to_serializable(
         players: PlayersSerializable,
         special_tiles: SpecialTilesSerializable,
         pieces: PiecesSerializable,
-        blockades: Blockades,
+        blockades: BlockadesSerializable,
         current_player: CurrentPlayer,
         dice_rolled: DiceRolled,
         processing_move: ProcessingMove,
@@ -52,37 +40,50 @@ game_state_to_serializable(
     }) :-
     maplist(player_to_serializable, Players, PlayersSerializable),
     maplist(special_tile_to_serializable, SpecialTiles, SpecialTilesSerializable),
-    maplist(piece_to_serializable, Pieces, PiecesSerializable).
+    maplist(piece_to_serializable, Pieces, PiecesSerializable),
+    convert_blockades(Blockades, BlockadesSerializable).
 
+% Serialização individual dos elementos
 player_to_serializable(player(Color, IsBot, StartPos), [Color, IsBot, StartPos]).
 special_tile_to_serializable(special_tile(Type, Position), [Type, Position]).
-piece_to_serializable(piece(Number, Color, Position, Walked, InStart, InFinish, Finished),
-                      [Number, Color, Position, Walked, InStart, InFinish, Finished]).
+piece_to_serializable(
+    piece(Number, Color, Position, Walked, InStart, InFinish, Finished),
+    [Number, Color, Position, Walked, InStart, InFinish, Finished]).
 
-% -----------------------
-% DESSERIALIZAÇÃO DO JSON
-% -----------------------
+% Converte blockades (lista de tuplas) para lista de dicts JSON
+convert_blockades([], []).
+convert_blockades([(Color, Tile)|Rest], [json{color: Color, tile: Tile}|ConvertedRest]) :-
+    convert_blockades(Rest, ConvertedRest).
 
-serializable_to_game_state(Dict, GameState) :-
-    (Dict = json(List) -> dict_create(RealDict, _, List) ; RealDict = Dict),
-    get_dict(players, RealDict, PlayersSerializable),
-    get_dict(special_tiles, RealDict, SpecialTilesSerializable),
-    get_dict(pieces, RealDict, PiecesSerializable),
-    get_dict(blockades, RealDict, Blockades),
-    get_dict(current_player, RealDict, CurrentPlayer),
-    get_dict(dice_rolled, RealDict, DiceRolled),
-    get_dict(processing_move, RealDict, ProcessingMove),
-    get_dict(end, RealDict, End),
-    get_dict(sixes_in_row, RealDict, SixesInRow),
-    get_dict(was_lucky_move, RealDict, WasLuckyMove),
-    get_dict(winner_color, RealDict, WinnerColor),
+% Reverso: transforma JSON lido em GameState
+serializable_to_game_state(
+    json{
+        players: PlayersSerializable,
+        special_tiles: SpecialTilesSerializable,
+        pieces: PiecesSerializable,
+        blockades: BlockadesSerializable,
+        current_player: CurrentPlayer,
+        dice_rolled: DiceRolled,
+        processing_move: ProcessingMove,
+        end: End,
+        sixes_in_row: SixesInRow,
+        was_lucky_move: WasLuckyMove,
+        winner_color: WinnerColor
+    },
+    game_state(Players, SpecialTiles, Pieces, Blockades, CurrentPlayer, DiceRolled,
+               ProcessingMove, End, SixesInRow, WasLuckyMove, WinnerColor)) :-
     maplist(serializable_to_player, PlayersSerializable, Players),
     maplist(serializable_to_special_tile, SpecialTilesSerializable, SpecialTiles),
     maplist(serializable_to_piece, PiecesSerializable, Pieces),
-    GameState = game_state(Players, SpecialTiles, Pieces, Blockades, CurrentPlayer, DiceRolled,
-                           ProcessingMove, End, SixesInRow, WasLuckyMove, WinnerColor).
+    convert_blockades_back(BlockadesSerializable, Blockades).
 
 serializable_to_player([Color, IsBot, StartPos], player(Color, IsBot, StartPos)).
 serializable_to_special_tile([Type, Position], special_tile(Type, Position)).
-serializable_to_piece([Number, Color, Position, Walked, InStart, InFinish, Finished],
-                      piece(Number, Color, Position, Walked, InStart, InFinish, Finished)).
+serializable_to_piece(
+    [Number, Color, Position, Walked, InStart, InFinish, Finished],
+    piece(Number, Color, Position, Walked, InStart, InFinish, Finished)).
+
+% Converte lista de dicts JSON em lista de tuplas para o Prolog
+convert_blockades_back([], []).
+convert_blockades_back([json{color: Color, tile: Tile}|Rest], [(Color, Tile)|ConvertedRest]) :-
+    convert_blockades_back(Rest, ConvertedRest).
