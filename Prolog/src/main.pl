05daea62a70922372002e36942c665aa9c202979
leonardo-mota-filho <leonardomotamfilho@gmail.com>
2025-04-09 22:36:08 -0300

    correção do erro de formatação

--- Prolog/src/main.pl
+++ Prolog/src/main.pl
@@ -54,32 +54,27 @@ player_turn(game_state(Players, SpecialTiles, Pieces, Blockades, CurrentPlayer, 
             TempGameState
         ),
         auxiliary:getToFromMove(MoveToPlay,To),
-
         ( member(special_tile(Type, To), SpecialTiles) ->
-        ( Type = lucky ->
+            ( Type = lucky ->
             write("\nSua peça caiu na casa Lucky! Selecione a posição de uma peça inimiga para trazer de volta à base (0 - n): "), 
             auxiliary:get_enemies(Pieces,CurrentPlayer,Enemies),
             auxiliary:get_pieces_locations(Enemies,Locations),
             write(Locations),
             read(luckyTargetPos), 
-            %Eliminar peça
             write("\nPeça inimiga retornada à base com sucesso!")
-        ; Type = boost ->
+            ; Type = boost ->
             write("\nBoost ativado! Sua peça andará 3 casas a mais automaticamente.\n")
-            % Aqui você pode adicionar lógica para movimentar 3 casas extras se quiser
-        ; Type = death ->
+            ; Type = death ->
             write("\nOh não! Sua peça caiu na casa da Morte e voltará para a base.\n")
-            % Aqui você pode aplicar a lógica de retornar a peça para a base
-        ; Type = decline ->
+            ; Type = decline ->
             write("\nVocê caiu numa casa Decline. Algo de ruim pode acontecer aqui...\n")
-            % Lógica de decline (se tiver)
-        ; Type = safe ->
+            ; Type = safe ->
             write("\nCasa segura! Nenhum jogador pode ser capturado aqui.\n")
         ; true
-        )
-        ; true
-        )
-    ),
+    )
+; true
+)
+    )),
     write("Passando para o proximo turno(Digite qualquer coisa para prosseguir)"), nl,
     write("Caso queira encerrar a partida digite fim"),
 
